9fb3926ddb0734d3e2630efe893573cbbc495bcf
Matthias Wolf <matze.wolf@tum.de>
2019-06-07 16:34:47 +0200

    Change dataset directory and small fixes

--- preprocessing.py
+++ preprocessing.py
@@ -3,13 +3,16 @@ import numpy as np
 import warnings
 warnings.filterwarnings("ignore")
 
-data = pd.read_csv('./fer2013.csv')
+data = pd.read_csv('../fer2013-data/fer2013.csv')
+''' Save and extract data from 
+https://www.kaggle.com/c/challenges-in-representation-learning-facial-expression-recognition-challenge/data
+to a 'fer2013-data' directory one layer above this Git repository. '''
 
 width, height = 48, 48
 
 datapoints = data['pixels'].tolist()
 
-#getting features for training
+# getting features for training
 X = []
 for xseq in datapoints:
     xx = [int(xp) for xp in xseq.split(' ')]
@@ -19,15 +22,15 @@ for xseq in datapoints:
 X = np.asarray(X)
 X = np.expand_dims(X, -1)
 
-#getting labels for training
+# getting labels for training
 y = pd.get_dummies(data['emotion']).as_matrix()
 
-#storing them using numpy
+# storing them using numpy
 np.save('fdataX', X)
 np.save('flabels', y)
 
 print("Preprocessing Done")
-print("Number of Features: "+str(len(X[0])))
-print("Number of Labels: "+ str(len(y[0])))
-print("Number of examples in dataset:"+str(len(X)))
+print("Number of Features: " + str(len(X[0])))
+print("Number of Labels: " + str(len(y[0])))
+print("Number of examples in dataset:" + str(len(X)))
 print("X,y stored in fdataX.npy and flabels.npy respectively")
