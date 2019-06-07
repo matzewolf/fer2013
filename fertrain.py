57268a70c670c08c55e7606755421d68795b8585
Matthias Wolf <matze.wolf@tum.de>
2019-06-07 17:15:08 +0200

    Add shebang and small fixes

--- fertrain.py
+++ fertrain.py
@@ -1,4 +1,7 @@
-import sys, os
+#!/usr/bin/env python3
+
+import sys
+import os
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
@@ -30,15 +33,15 @@ x /= np.std(x, axis=0)
 #    plt.imshow(x[xx].reshape((48, 48)), interpolation='none', cmap='gray')
 #plt.show()
 
-#splitting into training, validation and testing data
+# splitting into training, validation and testing data
 X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.1, random_state=42)
 X_train, X_valid, y_train, y_valid = train_test_split(X_train, y_train, test_size=0.1, random_state=41)
 
-#saving the test samples to be used later
+# saving the test samples to be used later
 np.save('modXtest', X_test)
 np.save('modytest', y_test)
 
-#desinging the CNN
+# desinging the CNN
 model = Sequential()
 
 model.add(Conv2D(num_features, kernel_size=(3, 3), activation='relu', input_shape=(width, height, 1), data_format='channels_last', kernel_regularizer=l2(0.01)))
@@ -81,12 +84,12 @@ model.add(Dense(num_labels, activation='softmax'))
 
 #model.summary()
 
-#Compliling the model with adam optimixer and categorical crossentropy loss
+# Compliling the model with adam optimixer and categorical crossentropy loss
 model.compile(loss=categorical_crossentropy,
               optimizer=Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-7),
               metrics=['accuracy'])
 
-#training the model
+# training the model
 model.fit(np.array(X_train), np.array(y_train),
           batch_size=batch_size,
           epochs=epochs,
@@ -94,7 +97,7 @@ model.fit(np.array(X_train), np.array(y_train),
           validation_data=(np.array(X_valid), np.array(y_valid)),
           shuffle=True)
 
-#saving the  model to be used later
+# saving the  model to be used later
 fer_json = model.to_json()
 with open("fer.json", "w") as json_file:
     json_file.write(fer_json)
